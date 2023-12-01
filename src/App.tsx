fafdfd1f296c219cab965ffc4414433c2b6373af
xing-6464 <xing_6464@foxmail.com>
2023-12-01 20:18:54 +0800

    添加 Button 基本样式

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ function App() {
       <Button btnType="danger" size="lg">
         hello
       </Button>
+      <Button disabled btnType="danger" size="lg">
+        hello
+      </Button>
+
       <Button btnType="link" size="lg" href="1.94.45.254:9002">
         跳转
       </Button>
