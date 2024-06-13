6ab1e77b94e4e03cf6a9b016b1ff7b0d5af3a767
xing-6464 <17621404964@163.com>
2024-06-13 15:05:36 +0800

    Button 测试用例第二部分

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,11 +1,36 @@
-import { describe, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './Button'
+const fn = vi.fn()
 
 describe('Button.tsx', () => {
   test('basic rendering', () => {
-    render(<Button type="primary">button</Button>)
+    render(
+      <Button type="primary" onClick={fn}>
+        button
+      </Button>,
+    )
+    const button = screen.getByRole('button')
+    expect(button.className).toContain('x-button--primary')
 
-    console.log(screen)
+    expect(button.textContent).toBe('button')
+
+    fireEvent.click(button)
+    expect(fn).toHaveBeenCalledOnce()
+  })
+
+  test('disabled button', () => {
+    fn.mockClear()
+    render(
+      <Button type="primary" onClick={fn} disabled>
+        button
+      </Button>,
+    )
+
+    const button = screen.getByRole('button') as HTMLButtonElement
+    expect(button.disabled).toBeTruthy()
+
+    fireEvent.click(button)
+    expect(fn).not.toHaveBeenCalled()
   })
 })
